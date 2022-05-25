c55fd0466072792caf081af5fa304ef8eacd898d
Markus Koenig <mkoenig2@onesubsea.slb.com>
2022-05-25 13:35:21 +0200

    Finalize basic task manager

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,45 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def list
     @tasks = Task.all
   end
 
-  def show
+  def show; end
+
+  def edit; end
+
+
+  def update
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task.destroy
+
+    # no need for app/views/restaurants/destroy.html.erb
+    redirect_to tasks_path
+  end
+
+  private
+
+  def set_task
     @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
